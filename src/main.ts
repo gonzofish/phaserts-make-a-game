29810321dff3416415cc75e4d5fd388934ffa131
Matt Fehskens <matt.fehskens@gmail.com>
2023-07-19 20:15:00 -0400

    Load assets
    Remove scaffold assets & code

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,30 @@
-import Phaser from "phaser";
-import HelloScene from "./scenes/HelloScene";
+import { AUTO, Game, Scene, Types } from 'phaser';
 
-const config: Phaser.Types.Core.GameConfig = {
-    parent: "app",
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scale: {
-        mode: Phaser.Scale.ScaleModes.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-    },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
-    scene: [
-        HelloScene,
-    ]
+class MainScene extends Scene {
+  create() {
+    this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'star');
+  }
+  preload() {
+    this.load.image('bomb', 'assets/images/bomb.png');
+    this.load.image('ground', 'assets/images/platform.png');
+    this.load.image('sky', 'assets/images/sky.png');
+    this.load.image('star', 'assets/images/star.png');
+
+    this.load.spritesheet('dude', 'assets/sheets/dude.png', {
+      frameHeight: 48,
+      frameWidth: 32,
+    });
+  }
+  update() {}
+}
+
+const config: Types.Core.GameConfig = {
+  height: 600,
+  parent: 'app',
+  scene: [MainScene],
+  type: AUTO,
+  width: 800,
 };
 
-export default new Phaser.Game(config);
+export default new Game(config);
