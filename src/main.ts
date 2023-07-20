0b2737df6822e03985475419cdcb064eec177508
Matt Fehskens <matt.fehskens@gmail.com>
2023-07-19 21:24:41 -0400

    Load character + add animations

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@ import { AUTO, Game, Physics, Scene, Types } from 'phaser';
 
 class MainScene extends Scene {
   private platforms: Physics.Arcade.StaticGroup | undefined;
+  private player: Types.Physics.Arcade.SpriteWithDynamicBody | undefined;
 
   create() {
     // positions are via the center; so this asset is positioned so its center is at 400x300
     this.add.image(400, 300, 'sky');
 
+    this.setupPlatforms();
+    this.setupPlayer();
+  }
+
+  private setupPlatforms() {
     // Add a group of static bodies (those not affected by physics) to the scene
     this.platforms = this.physics.add.staticGroup();
     // scale this one so it's the full width of the viewport
@@ -18,6 +24,45 @@ class MainScene extends Scene {
     this.platforms.create(50, 250, 'ground');
     this.platforms.create(750, 220, 'ground');
   }
+
+  private setupPlayer() {
+    this.setupPlayerSprite();
+    this.setupPlayerAnimations();
+  }
+
+  private setupPlayerSprite() {
+    this.player = this.physics.add.sprite(100, 450, 'dude');
+    // when the player jumps they'll bounce just a little bit
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+  }
+
+  private setupPlayerAnimations() {
+    // moving left animation
+    this.anims.create({
+      frameRate: 10,
+      // generate the frames for the animation using slice 0 to 3 of the dude spritesheet
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      key: 'left',
+      repeat: -1,
+    });
+
+    // turning/direction change animation
+    this.anims.create({
+      frameRate: 20,
+      frames: [{ frame: 4, key: 'dude' }],
+      key: 'turn',
+    });
+
+    // moving right animation
+    this.anims.create({
+      frameRate: 10,
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      key: 'right',
+      repeat: -1,
+    });
+  }
+
   preload() {
     this.load.image('bomb', 'assets/images/bomb.png');
     this.load.image('ground', 'assets/images/platform.png');
