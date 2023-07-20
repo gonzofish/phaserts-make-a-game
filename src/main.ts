6f045cd0f8bc7e23d14df947a4b88adbe35db81e
Matt Fehskens <matt.fehskens@gmail.com>
2023-07-19 20:18:44 -0400

    Add Prettier

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,26 @@
-import { AUTO, Game, Scene, Types } from 'phaser';
+import { AUTO, Game, Scene, Types } from "phaser";
 
 class MainScene extends Scene {
   create() {
-    this.add.image(400, 300, 'sky');
-    this.add.image(400, 300, 'star');
+    this.add.image(400, 300, "sky");
+    this.add.image(400, 300, "star");
   }
   preload() {
-    this.load.image('bomb', 'assets/images/bomb.png');
-    this.load.image('ground', 'assets/images/platform.png');
-    this.load.image('sky', 'assets/images/sky.png');
-    this.load.image('star', 'assets/images/star.png');
+    this.load.image("bomb", "assets/images/bomb.png");
+    this.load.image("ground", "assets/images/platform.png");
+    this.load.image("sky", "assets/images/sky.png");
+    this.load.image("star", "assets/images/star.png");
 
-    this.load.spritesheet('dude', 'assets/sheets/dude.png', {
+    this.load.spritesheet("dude", "assets/sheets/dude.png", {
       frameHeight: 48,
       frameWidth: 32,
     });
   }
-  update() {}
 }
 
 const config: Types.Core.GameConfig = {
   height: 600,
-  parent: 'app',
+  parent: "app",
   scene: [MainScene],
   type: AUTO,
   width: 800,
